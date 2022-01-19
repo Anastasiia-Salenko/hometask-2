c21628ffa860632fba5c42a27c28baadfe1251f1
Anastasiia Salenko <batsvinova@gmail.com>
2022-01-19 22:33:23 +0200

    Refactored note item reducer

--- src/features/note-item/redux/action-handlers.js
+++ src/features/note-item/redux/action-handlers.js
@@ -4,60 +4,38 @@ import {
   SAVE_NOTE_BUTTON_CLICKED,
 } from "./constants";
 
+const changeNote = (itemReducer) => {
+  return (state, action) => {
+    const notes = state.notes.map((item) => {
+      if (item.id === action.payload.id) {
+        return itemReducer(item, action);
+      }
+
+      return item;
+    });
+
+    return {
+      ...state,
+      notes,
+    };
+  };
+};
+
 export const actionHandlers = {
-  [CHANGE_NOTE_CONTENT]: (state, action) => {
-    const notes = state.notes.map((item) => {
-      if (item.id === action.payload.id) {
-        return {
-          ...item,
-          content: action.payload.content,
-        };
-      }
-
-      return item;
-    });
-
-    return {
-      ...state,
-      notes,
-    };
-  },
-  [SAVE_NOTE_BUTTON_CLICKED]: (state, action) => {
-    const notes = state.notes.map((item) => {
-      if (item.id === action.payload.id) {
-        return {
-          ...item,
-          isDraft: false,
-          isEditing: false,
-          created: item.created || action.payload.date,
-        };
-      }
-
-      return item;
-    });
-
-    return {
-      ...state,
-      notes,
-    };
-  },
-  [EDIT_NOTE_BUTTON_CLICKED]: (state, action) => {
-    const notes = state.notes.map((item) => {
-      if (item.id === action.payload.id) {
-        return {
-          ...item,
-          contentOriginal: item.content,
-          categoryOriginal: item.category,
-          isEditing: true,
-        };
-      }
-
-      return item;
-    });
-
-    return {
-      ...state,
-      notes,
-    };
-  },
+  [CHANGE_NOTE_CONTENT]: changeNote((note, action) => ({
+    ...note,
+    content: action.payload.content,
+  })),
+  [SAVE_NOTE_BUTTON_CLICKED]: changeNote((note, action) => ({
+    ...note,
+    isDraft: false,
+    isEditing: false,
+    created: note.created || action.payload.date,
+  })),
+  [EDIT_NOTE_BUTTON_CLICKED]: changeNote((note) => ({
+    ...note,
+    contentOriginal: note.content,
+    categoryOriginal: note.category,
+    isEditing: true,
+  })),
 };
